5dcd181bd0d036a23a630b164df5b316a356fd33
Callum Coy <callumcoy@ymail.com>
2025-01-24 14:55:37 -0500

    Splits the image into a 3 by 3 matrix

--- SymbolTraining.py
+++ SymbolTraining.py
@@ -7,14 +7,18 @@ FONT_SIZE = 1
 def main():
 
     img = createImage("M")
-    cv.resize(img, None, fx=10, fy=10)
+    splitImage = imageSplitter(img)
 
-    cv.imshow("edges", cv.resize(img, None, fx=10, fy=10))
+    for y in range(splitImage.shape[0]):
+        for x in range(splitImage.shape[1]):
+            cv.imshow("Piece" + str(x) + str(y),
+                      cv.resize(splitImage[y, x], None, fx=10, fy=10))
+
     cv.waitKey(0)
 
 
 def createImage(letter):
-    # Gets sizing of the input
+    # Gets sizing of the input.
     font = cv.FONT_HERSHEY_SIMPLEX
     textWidth, textHeight = cv.getTextSize(letter, font, FONT_SIZE, 2)[0]
 
@@ -23,10 +27,23 @@ def createImage(letter):
     img.fill(255)
 
     # Puts the letter centered on a blank canvas.
-    cv.putText(img, letter, (0, textHeight//2), font, FONT_SIZE/2, (0, 0, 0), 1, cv.LINE_AA)
+    cv.putText(img, letter, (0, textHeight//2), font,
+               FONT_SIZE/2, (0, 0, 0), 1, cv.LINE_AA)
 
     # Grey scales the image.
     return cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+
+def imageSplitter(img, blocks=(3, 3)):
+    # Splits the image in x rows, usually 3.
+    horizontalSplit = np.array_split(img, blocks[0])
+
+    # Splits the x rows into y sections, usually 3.
+    splitImage = [np.array_split(block, blocks[1], axis=1)
+                  for block in horizontalSplit]
+
+    # Makes an array then sorts it into a 2d array.
+    return np.asarray(splitImage, dtype=np.ndarray).reshape(blocks)
 
 
 main()
